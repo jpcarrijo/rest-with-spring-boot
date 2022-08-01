359bad373a9011fff8e13293a0de93d35b9484f2
João Paulo <carrijojp@gmail.com>
2022-08-01 14:11:45 -0300

    endpoint greeting

---
+++ src/test/java/com/spring/restwithspringboot/StartupTests.java
@@ -0,0 +1,13 @@
+package com.spring.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
